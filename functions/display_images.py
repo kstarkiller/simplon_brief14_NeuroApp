1efe5bcbadd31ba5c2d147b9a7cf417d1e5099b9
Kevin SIMON <simonkvin@gmail.com>
2024-02-26 16:57:30 +0100

    Initial commit

---
+++ functions/display_images.py
@@ -0,0 +1,29 @@
+import math
+import matplotlib.pyplot as plt
+
+def display_images(X, y, n):
+    # Filtrer les images par classe
+    yes_images =  X[y == 1][:n]
+    no_images = X[y == 0][:n]
+
+    # Calculer le nombre de lignes et colonnes pour chaque classe
+    n_rows = int(math.sqrt(n))
+    n_cols = math.ceil(n / n_rows)
+
+    # Créer la figure pour la classe "yes"
+    fig_yes, axs_yes = plt.subplots(n_rows, n_cols, figsize=(12, 10))
+    fig_yes.suptitle('Class: Yes')
+
+    # Afficher des images de la classe "yes"
+    for i in range(min(n, len(yes_images))):
+        axs_yes[i // n_cols, i % n_cols].imshow(yes_images[i])
+
+    # Créer la figure pour la classe "no"
+    fig_no, axs_no = plt.subplots(n_rows, n_cols, figsize=(12, 10))
+    fig_no.suptitle('Class: No')
+
+    # Afficher des images de la classe "no"
+    for i in range(min(n, len(no_images))):
+        axs_no[i // n_cols, i % n_cols].imshow(no_images[i])
+
+    plt.show()
---
+++ model_api.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+import numpy as np
+import mlflow
+import mlflow.keras
+from PIL import Image
+import io
+import cv2
+
+from functions.normalize_images import normalize_image
+
+# Load the model
+mlflow.set_tracking_uri('http://localhost:5000')
+model = mlflow.pyfunc.load_model('runs:/cd2378db4d16452a831d248eea8811a7/models')
+
+app = FastAPI()
+
+@app.post("/predict/")
+async def predict(file: UploadFile = File(...)):
+    # Check if the file is an image
+    if file is None:
+        return JSONResponse(status_code=400, content={"message": "No file uploaded."})
+    
+    # Read the image
+    image_data = await file.read()
+    # convert the bytes to image
+    image_data = np.frombuffer(image_data, np.uint8)
+    # decode the bytes to image
+    decoded_image = cv2.imdecode(image_data, cv2.IMREAD_COLOR)
+
+    # Normalize the image
+    image_ready = normalize_image(decoded_image, (224, 224))
+    # Reshape and convert the image to a list of images (batch of 1)
+    image_ready = np.array(image_ready).reshape(1, 224, 224, 3)
+
+    # Make a prediction
+    prediction = model.predict(image_ready)
+    # Format the prediction
+    pred_label = "yes" if prediction[0][0] > 0.5 else "no"
+
+    return {"label": pred_label, "confidence": float(prediction[0][0])}
+
+# Run the API with uvicorn
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
